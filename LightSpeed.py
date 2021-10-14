0a26878a55dcbcf3c262b6dc0fac3cbe53e51a34
samsuy0304 <71359041+samsuy0304@users.noreply.github.com>
2021-10-14 14:36:29 -0500

    Add files via upload

--- LightSpeed.py
+++ LightSpeed.py
@@ -16,7 +16,7 @@ time_differences_all = []
 #later we take the average of each of these
 #so you get the average time shift for a distance
 #which gives you the five values we plot at the end
-for x in [18,24,30,36,40]:
+for x in [18,24,30,36]:
     x = str(x)
     time_difs = []
     for y in [4,5,6,7,8]:
@@ -29,8 +29,8 @@ for x in [18,24,30,36,40]:
 
         #I just multiplied by 10**7 to get better intuition about the plots
         #I undo it later by multiplying by 10**-7
-        plt.plot(curve1[3]*(10**7), curve1[4])
-        plt.plot(curve2[3]*(10**7), curve2[4]/curve2_scale)
+        plt.plot(curve1[3], curve1[4])
+        plt.plot(curve2[3], curve2[4]/curve2_scale)
 
         plt.ylim(-6,6)
         plt.xlim(-3,3)
@@ -38,42 +38,62 @@ for x in [18,24,30,36,40]:
         g = np.where(curve1[4] == 0)
         f = np.where(curve2[4] == 0)
 
-        plt.plot(curve1[3][g[0]]*(10**7),curve1[3][g[0]]*0,'bo')
-        plt.plot(curve2[3][f[0]]*(10**7),curve2[3][f[0]]*0,'ro')
+        plt.plot(curve1[3][g[0]],curve1[3][g[0]],'bo')
+        plt.plot(curve2[3][f[0]],curve2[3][f[0]],'ro')
 
         plt.xlabel('$/Delta$t')
         plt.title('Phase Shift of the Waves ' + x + ' feet and ' + y + ' MHz')
-        plt.savefig('fig')
+        plt.savefig('Phase Shift of the Waves ' + x + ' feet and ' + y + ' MHz')
         #plt.show()
         for n in [0]:
-            shift = np.abs(curve2[3][f[0][n]]*(10**7) - curve1[3][g[0][n]]*(10**7))
-            time_difs = np.append(time_difs,shift)
+            shift = np.abs(curve2[3][f[0][n]] - curve1[3][g[0][n]])
+            time_difs = np.append(time_difs,shift)   #
     time_differences_all.append(time_difs)
+
+print(time_differences_all)
+
 
 #Doing what i talked about at the beginning
 time_differences_averages = []
+stda = []
 for x in time_differences_all:
-    mean = np.mean(x)*(10**(-7))
+    mean = np.average(x, weights=[0.1,1,0.2,1,0.2])
+    std = np.std(x)
     time_differences_averages.append(mean)
+    stda.append(std)
 
-dist_m = np.array([40*0.3,36*0.3,30*0.3,24*0.3,18*0.3])
+print("average",time_differences_averages)
+print("standard",stda)
+k=[18*0.3,24*0.3,30*0.3,36*0.3]
+re =[40*0.3,36*0.3,30*0.3,24*0.3,18*0.3]
+dist_m = np.array(k)
 fig0 = plt.figure(figsize=(8,4))
 fig0.patch.set_facecolor('xkcd:black')
 plt.style.use('dark_background')
 ax0=plt.subplot(1,2,1)
-ax0.plot(time_differences_averages,dist_m*2,'ro')
+
 #plt.xlabel('$\Delta$ t')
 #plt.ylabel('Distance in meters')
-print('this should be linear wtf???? is going on????')
-plt.show()
+
+#plt.show()
 
 
 
 
+theory = (dist_m)/2.99702547
+exact = np.array(time_differences_averages)#*10**8
+print("theory",theory)
+print("exact",exact)
 
 
 
+d = ((exact-theory)**2)/theory
 
+
+print(sum(d))
+
+
+ax0.plot(dist_m,exact,'bo')
 
 
 
@@ -88,24 +108,24 @@ from pprint import pprint
 from iminuit import Minuit, __version__
 print('iminuit version', __version__)
 
-data_x = np.array(time_differences_averages)
-data_y = dist_m
-#ysig
+data_x = dist_m
+data_y = exact
+ysig = np.array(stda)#*10**8
 #errorbar(data_x,data_y,yerr=ysig,fmt="o",color='k',solid_capstyle='projecting',capsize=5)
 
 #print(data_y)
 #def model(x, a, b, c,d):
 def model(x, a):
-    return x*a
+    return x*(1/a)
 
 def least_squares(a):
 #def least_squares(a,b,c,d):
-    return sum((data_y - model(data_x,a))**2)# / ysig**2)
+    return sum(((data_y - model(data_x,a))**2 / ysig**2 ))#
     #return sum((data_y - model(data_x,a,b,c,d))**2 / ysig**2)
 
 
 
-m = Minuit(least_squares, 300000000)
+m = Minuit(least_squares, 2.99702547 )
 #m = Minuit(least_squares, 200,180,0.45,0.55)
 #m = Minuit(least_squares, 5.29,5.01,2.0,2.5)
 m.migrad() # finds minimum of least_squares function
@@ -114,7 +134,7 @@ print(' ')
 print(' Covariance matrix')
 #pprint(m.covariance())
 print(' ')
-#plt.plot(model(data_x, *m.values), 'r-')
+#plt.plot(model(data_x, *m.values), 'b-')
 plt.plot(data_x, model(data_x, *m.values), 'r-')
 print(' ')
 print(' Fitting parameters')
