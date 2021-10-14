67d4f3caf739c29f8d73b1dbae02bfcf55f4b6df
samsuy0304 <71359041+samsuy0304@users.noreply.github.com>
2021-10-14 11:22:19 -0500

    Add files via upload

---
+++ LightSpeed.py
@@ -0,0 +1,130 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from scipy.optimize import minimize, rosen, rosen_der
+from scipy import interpolate
+import scipy
+import os
+from scipy.optimize import curve_fit
+from scipy.integrate import simps
+import pandas as pd
+
+time_differences_all = []
+#Nested loop so that you can do process for each distance
+#and each frequency
+#So you have a list of lists, where each list inside the bigger list
+#is the time shift for each frequency at a distance
+#later we take the average of each of these
+#so you get the average time shift for a distance
+#which gives you the five values we plot at the end
+for x in [18,24,30,36,40]:
+    x = str(x)
+    time_difs = []
+    for y in [4,5,6,7,8]:
+        y = str(y)
+        curve1 = pd.read_csv("C:/Users/suyas/github/Light Speed/New_Light/" + x + "/" + y + "/ALL0000/F0000CH1.csv",header=None)
+        curve2 = pd.read_csv("C:/Users/suyas/github/Light Speed/New_Light/" + x + "/" + y + "/ALL0000/F0000CH2.csv",header=None)
+
+        #making them on the same scale from the data
+        curve2_scale = float(curve2[1][8])/float(curve1[1][8])
+
+        #I just multiplied by 10**7 to get better intuition about the plots
+        #I undo it later by multiplying by 10**-7
+        plt.plot(curve1[3]*(10**7), curve1[4])
+        plt.plot(curve2[3]*(10**7), curve2[4]/curve2_scale)
+
+        plt.ylim(-6,6)
+        plt.xlim(-3,3)
+
+        g = np.where(curve1[4] == 0)
+        f = np.where(curve2[4] == 0)
+
+        plt.plot(curve1[3][g[0]]*(10**7),curve1[3][g[0]]*0,'bo')
+        plt.plot(curve2[3][f[0]]*(10**7),curve2[3][f[0]]*0,'ro')
+
+        plt.xlabel('$/Delta$t')
+        plt.title('Phase Shift of the Waves ' + x + ' feet and ' + y + ' MHz')
+        plt.savefig('fig')
+        #plt.show()
+        for n in [0]:
+            shift = np.abs(curve2[3][f[0][n]]*(10**7) - curve1[3][g[0][n]]*(10**7))
+            time_difs = np.append(time_difs,shift)
+    time_differences_all.append(time_difs)
+
+#Doing what i talked about at the beginning
+time_differences_averages = []
+for x in time_differences_all:
+    mean = np.mean(x)*(10**(-7))
+    time_differences_averages.append(mean)
+
+dist_m = np.array([40*0.3,36*0.3,30*0.3,24*0.3,18*0.3])
+fig0 = plt.figure(figsize=(8,4))
+fig0.patch.set_facecolor('xkcd:black')
+plt.style.use('dark_background')
+ax0=plt.subplot(1,2,1)
+ax0.plot(time_differences_averages,dist_m*2,'ro')
+#plt.xlabel('$\Delta$ t')
+#plt.ylabel('Distance in meters')
+print('this should be linear wtf???? is going on????')
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from matplotlib import pyplot as plt
+import numpy as np
+from pylab import *
+from pprint import pprint
+
+from iminuit import Minuit, __version__
+print('iminuit version', __version__)
+
+data_x = np.array(time_differences_averages)
+data_y = dist_m
+#ysig
+#errorbar(data_x,data_y,yerr=ysig,fmt="o",color='k',solid_capstyle='projecting',capsize=5)
+
+#print(data_y)
+#def model(x, a, b, c,d):
+def model(x, a):
+    return x*a
+
+def least_squares(a):
+#def least_squares(a,b,c,d):
+    return sum((data_y - model(data_x,a))**2)# / ysig**2)
+    #return sum((data_y - model(data_x,a,b,c,d))**2 / ysig**2)
+
+
+
+m = Minuit(least_squares, 300000000)
+#m = Minuit(least_squares, 200,180,0.45,0.55)
+#m = Minuit(least_squares, 5.29,5.01,2.0,2.5)
+m.migrad() # finds minimum of least_squares function
+m.hesse() # computes errors
+print(' ')
+print(' Covariance matrix')
+#pprint(m.covariance())
+print(' ')
+#plt.plot(model(data_x, *m.values), 'r-')
+plt.plot(data_x, model(data_x, *m.values), 'r-')
+print(' ')
+print(' Fitting parameters')
+for p in m.parameters:
+    print("{} = {} +- {}".format(p,m.values[p], m.errors[p]))
+
+print(' ')
+print('chi square:', m.fval)
+print('number of degrees of freedom:', (len(data_y)- len(m.parameters)))
+print('reduced chi square:', m.fval / (len(data_y) - len(m.parameters)) )
+print(len(m.parameters))
+print(m.parameters)
+show()
